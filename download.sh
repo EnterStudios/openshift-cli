764d5355752fad037c048fd704830b44e712a566
Nick <nick@nine.ch>
2017-04-21 15:01:30 +0200

    adding debug information

--- download.sh
+++ download.sh
@@ -16,16 +16,24 @@ function debug {
   fi
 }
 
+debug "creating download directory"
 mkdir -p download
+
+debug "changing into download directory"
 pushd download
 
 # get the latest release of oc
 latest_url="$(curl https://api.github.com/repos/openshift/origin/releases/latest 2>/dev/null | jq -r '.assets[].browser_download_url' | grep -e 'client-tools-.*-linux-64bit.tar.gz')"
+debug "the URL for the latest release should be $latest_url"
 
 # download
+debug "downloading the archive"
 wget -q "$latest_url"
 
+debug "content of dir is: $(ls)"
+
 # extract the archive contents
+debug "extracting stuff..."
 tar --warning=none --strip=1 -xzf *.tar.gz
 
 popd
