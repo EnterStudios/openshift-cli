754b8a45542c2c84ab84b9e98b53ee766279c9be
Nick <nick@nine.ch>
2017-10-26 10:22:30 +0200

    allow to download a specific version

--- download.sh
+++ download.sh
@@ -7,6 +7,12 @@
 ### CONFIG
 DEBUG=${DEBUG:-0}
 
+if [ -n "$VERSION" ]; then
+  VER="tags/$VERSION"
+else
+  VER="latest"
+fi
+
 ### FUNCTIONS
 function debug {
   local msg="$1"
@@ -23,17 +29,17 @@ debug "changing into download directory"
 pushd download
 
 # get the latest release of oc
-latest_url="$(curl https://api.github.com/repos/openshift/origin/releases/latest 2>/dev/null | jq -r '.assets[].browser_download_url' | grep -e 'client-tools-.*-linux-64bit.tar.gz')"
+latest_url="$(curl https://api.github.com/repos/openshift/origin/releases/$VER 2>/dev/null | jq -r '.assets[].browser_download_url' | grep -e 'client-tools-.*-linux-64bit.tar.gz')"
 debug "the URL for the latest release should be $latest_url"
 
 # download
 debug "downloading the archive"
-wget -q "$latest_url"
+wget -q "$latest_url" -O oc.tar.gz
 
 debug "content of dir is: $(ls)"
 
 # extract the archive contents
 debug "extracting stuff..."
-tar --warning=none --strip=1 -xzf *.tar.gz
+tar --warning=none --strip=1 -xzf oc.tar.gz
 
 popd
